67ec2bf3bd67fd2759d59d77fd7e8450c00cdd07
lawMS <50967742+lawMS@users.noreply.github.com>
2020-08-25 18:59:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -103,9 +103,20 @@ void subMenuDirector() {
 		switch(opcion){
 				
 			case 1:	{
+				clase *auxArregloClases;
 				string codigoCurso;
 				string nombreEstudiante;
-				cout<<"Digite el codigo del curso"<<endl;
+				
+				int cont = manejo_archivo.contadorLineas("archivosBase","estudiantePorCurso");
+				auxArregloClases = manejo_archivo.consultarClases("archivosBase","estudiantePorCurso",cont);
+
+				int contA = manejo_archivo.contadorLineas("archivosBase","archivosPorCurso");
+				espacio *arregloCursos = manejo_archivo.consultarCursos(contA);
+				cout<<"Estos son los cursos registrados \n";
+				for(int a=0; a<contA; a++){
+						cout<<"  -" << arregloCursos[a].codigoEspacio<<"\n";
+					}
+   				cout<<"Digite el codigo del curso"<<endl;
 				cin>>codigoCurso;
 				cout<<"Digite el nombre del estudiante"<<endl;
 				cin>>nombreEstudiante;
